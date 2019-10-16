de251231a180906bc8b8175e49e78c4f44a131bf
Niall Ó Broin <niall@turbulentdynamics.com>
2019-10-16 11:24:07 +0200

    Added test target

--- Package.swift
+++ Package.swift
@@ -5,6 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "tdQVecTool",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "tdQVecLibrary",
+            targets: ["tdQVecCore"]),
+        .executable(
+            name: "tdQVecTool",
+            targets: ["tdQVecTool"])
+    ],
     dependencies: [
         .package(
             url: "https://github.com/apple/swift-log.git",
@@ -20,10 +29,10 @@ let package = Package(
             name: "tdQVecCore",
             dependencies: ["Logging"]
         ),
-//        .testTarget(
-//            name: "tdQVecToolTests",
-//            dependencies: ["tdQVecCore"]
-//        )
+        .testTarget(
+            name: "tdQVecCoreTests",
+            dependencies: ["tdQVecCore"]
+        )
     ]
 )
 
